1dc6305907ef165ba2aadaeee33d07b246e594d7
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-09-30 10:39:31 -0700

    Add un/managed examples of un/selecting squares

--- src/vuepoints.js
+++ src/vuepoints.js
@@ -39,6 +39,24 @@ Vue.component('lines-vuepoint', {
   '
 })
 
+Vue.component('squares-vuepoint', {
+  props: [
+    'squares', 'selectedSquareIds', 'squareIsSelected', 'react'
+  ],
+  template: '\
+    <svg style="height: 500; width: 1000;"\
+      @click="react(\'unselectAllSquares\')">\
+      <transition-group name="squares" tag="g">\
+        <rect v-for="(square, squareIndex) in squares" :key="square.id"\
+          @click.stop="react(\'toggleSquareSelected\', square.id)"\
+          :x="100 + 100 * squareIndex"\
+          y="100" width="50" height="50" rx="10" ry="10"\
+          :fill="square.selected ? \'green\' : \'black\'"/>\
+      </transition-group>\
+    </div>\
+  '
+})
+
 Vue.component('points-vuepoint', {
   props: ['points'],
   template: '\
@@ -53,16 +71,22 @@ Vue.component('points-vuepoint', {
 })
 
 function VuepointManager(wrapperId, state, reactions) {
+  var react = function(action) {
+    var reaction = reactions[action]
+    if (!reaction) {
+      console.log(action, 'is not a defined action.');
+      return
+    }
+    var args = [...arguments]
+    reaction(state, ...args.slice(1, args.length))
+  }
   new Vue({
     el: '#' + wrapperId,
     data: {
-      state: state
+      state: state,
+      react: react,
+      reactions: reactions
     }
   })
-  return {
-    react: function(action) {
-      var reaction = reactions[action]
-      reaction(state)
-    }
-  }
+  return { react }
 }
